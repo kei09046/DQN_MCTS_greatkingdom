1efc21dd7ddc3250c4cc513d5f06de70ea666a67
kei09046 <ssgreg32@gmail.com>
2025-11-30 09:14:23 +0900

    add annotation

--- run_train.sh
+++ run_train.sh
@@ -8,3 +8,9 @@ elif [ "$output" = "none" ]; then
 else
     ./train < "$input" > "$output"
 fi
+
+# usage : ./train run_train.txt
+# example run_train.txt :
+# train
+# modelg2000.pt (model name to start at, to start from beginning, type none)
+# 10000 4 0 (total games to train, number of threads, is_shown)
