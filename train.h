95846959a1334e60df6dfb2298512c41c5526343
kei09046 <ssgreg32@gmail.com>
2025-12-13 23:32:19 +0900

    model training

--- train.h
+++ train.h
@@ -6,7 +6,6 @@
 #include "neuralNet.h"
 #include "random.h"
 #include "rotation.h"
-#include "elo.h"
 #include "consts.h"
 #include <string>
 #include <deque>
@@ -33,9 +32,6 @@ private:
 	PolicyValueNet inference_model, train_model;
 	std::mutex buffer_mutex;
 
-	static std::vector<bool> play_match(MCTS* player_one, MCTS* player_two, // return result of the match 1 : win for player_one, 0 : win for player two
-		std::ostream& total_res, bool is_shown = false, float temp = 1.0f, int n_games = 100);
-
 	void pin_threads_to_core(std::thread& th, int core_id);
 
 public:
@@ -43,19 +39,6 @@ public:
 	std::array<float, inputChannel * batchSize * inputSize>* state_batch;
 	std::array<float, batchSize * outputSize>* nextmove_batch;
 	std::array<float, batchSize>* winner_batch;
-
-
-	static float start_play(std::array<MCTS*, 2> player_list, // two models play against each other
-		std::ostream& part_res, bool is_shown = false, float temp = 0.1f);
-
-	static void play(const std::string& model, color side, int playout, float temp, bool gpu, bool shown); // play against human
-
-	static float policy_evaluate(const std::string& mod_one, const std::string& mod_two, 
-		std::ostream& total_res, std::ostream& part_res, bool is_shown = false, bool gpu = true, float temp = 1.0f, int n_games = 100);
-
-	static std::vector<float> policy_evaluate(std::vector<std::string> model_list, // return list of elo
-		std::ostream& total_res, bool is_shown = false, bool gpu = true, float temp = 1.0f, int n_games = 100);
-
 
 	TrainPipeline(std::string init_model, std::string test_model, bool gpu = false);
 
